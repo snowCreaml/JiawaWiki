530d6ca459f5b9cd28a6273f23b1fee71fcccac6
ColdRain <tgsxljk@126.com>
2021-05-31 21:53:00 +0800

    2-8 增加了配置项示例

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,14 +1,15 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello}")
+    private String testHello;
 
     @GetMapping("/hello")
     public String hello() {
