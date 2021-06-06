e52e22a30d2bbfe2e560f35dafda283d32e16546
ColdRain <tgsxljk@126.com>
2021-06-06 16:35:23 +0800

    3-4 mybatis使用示例：开发/test/list接口，查询test表所有数据

---
+++ src/main/java/com/jiawa/wiki/mapper/TestMapper.java
@@ -0,0 +1,14 @@
+package com.jiawa.wiki.mapper;
+
+import com.jiawa.wiki.domain.Test;
+
+import java.util.List;
+
+/**
+ * @author ColdRain
+ */
+public interface TestMapper {
+
+    public List<Test> list();
+
+}
